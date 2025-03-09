e1ca38c1edba683ffb20a2877dfa5571fbddfdbe
Friggitello <fggabrielito@gmail.com>
2025-03-09 18:51:06 +0100

    cambiato dipendenteclient

--- services/newses/src/main/java/com/azienda/newses/services/PosizioneLavorativaClient.java
+++ services/newses/src/main/java/com/azienda/newses/services/PosizioneLavorativaClient.java
@@ -1,15 +1,11 @@
 package com.azienda.newses.services;
 
-import com.azienda.dipendenti.dtos.response.PosizioneLavorativaResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "newses-service", url = "${application.config.posizioni-lavorative-url}")
 public interface PosizioneLavorativaClient {
-
-    @GetMapping("/get/{id}")
-    PosizioneLavorativaResponse getPosizioneLavorativaById(@PathVariable Long id);
 
     @GetMapping("/type/{id}")
     String getNomePosizioneById(@PathVariable Long id);
